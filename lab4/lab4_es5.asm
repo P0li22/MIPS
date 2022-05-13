e3f8b2670adda1faaa12dbe322ddd70465f05001
Poli <lucapili01@gmail.com>
2022-05-13 16:41:03 +0200

    a

--- lab4/lab4_es5.asm
+++ lab4/lab4_es5.asm
@@ -3,8 +3,29 @@ mat: .word 154, 123, 109, 86, 4, 0
      .word 412, -23, -231, 9, 50, 0
      .word 123, -24, 12, 55, -45, 0
      .word 0, 0, 0, 0, 0, 0
+nRows: .byte 5
+nColumns: .byte 3
 
 .text
 .globl main
 .ent main
 main:
+la $s0, mat
+add $t0, $0, $0 # somma riga/colonna
+add $t1, $0, $0 # contatore cicli
+lw $t2, nRows
+
+rows:
+lw $t3, $s0 # valore corrente
+add $t0, $t0, $t3
+add $s0, $s0, 4 # aggiorna indirizzo
+addi $t1, $t1, 1 # aggiorna contatore
+bne $t1, $t2, rows
+
+lw $t2, nColumns
+la $s0, mat
+add $t0, $0, $0
+add $t1, $0, $0
+
+columns:
+lw $t3, $s0
