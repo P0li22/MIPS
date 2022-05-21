3e78be03a94ed481c000b46e48d71e82a2c52ec6
Poli <lucapili01@gmail.com>
2022-05-21 15:26:42 +0200

    a

--- lab4/lab4_es5.asm
+++ lab4/lab4_es5.asm
@@ -3,8 +3,8 @@ mat: .word 154, 123, 109, 86, 4, 0
      .word 412, -23, -231, 9, 50, 0
      .word 123, -24, 12, 55, -45, 0
      .word 0, 0, 0, 0, 0, 0
-nRows: .byte 5
-nColumns: .byte 3
+nRows: .byte 3
+nColumns: .byte 5
 
 .text
 .globl main
@@ -12,20 +12,63 @@ nColumns: .byte 3
 main:
 la $s0, mat
 add $t0, $0, $0 # somma riga/colonna
-add $t1, $0, $0 # contatore cicli
-lw $t2, nRows
+add $t1, $0, $0 # contatore colonne
+add $t4, $0, $0 # contatore righe
+add $s1, $0, $0 # salva il valore da mettere nell'ultima posizione
+lb $t2, nRows
+lb $t5, nColumns
 
 rows:
-lw $t3, $s0 # valore corrente
+row:
+lw $t3, 0($s0) # valore corrente
 add $t0, $t0, $t3
-add $s0, $s0, 4 # aggiorna indirizzo
-addi $t1, $t1, 1 # aggiorna contatore
-bne $t1, $t2, rows
+addi $s0, $s0, 4 # aggiorna indirizzo
+addi $t1, $t1, 1 # aggiorna contatore colonna
+bne $t1, $t5, row
 
-lw $t2, nColumns
-la $s0, mat
+sw $t0, 0($s0)
+add $s1, $s1, $t0
+addi $s0, $s0, 4
 add $t0, $0, $0
 add $t1, $0, $0
+addi $t4, $t4, 1 # aggiorna contatore riga
+bne $t4, $t2, rows
+
+la $s0, mat
+add $t4, $0, $0
 
 columns:
-lw $t3, $s0
+column:
+lw $t3, 0($s0)
+add $t0, $t0, $t3
+mul $t6, $t5, 4
+add $s0, $s0, $t6
+addi $s0, $s0, 4 # salta alla riga successiva
+addi $t4, $t4, 1 # salta la cella che contiene la somma della riga
+bne $t4, $t2, column
+
+sw $t0, 0($s0)
+add $s1, $s1, $t0
+add $t0, $0, $0
+add $t4, $0, $0
+add $t1, $t1, 1
+mul $t6, $t1, 4
+la $s0, mat
+add $s0, $s0, $t6
+bne $t1, $t5, columns
+
+# salva la somma dell'ultima riga e dell'ultima colonna, calcolate con i
+# cicli precedenti, nell'ultima cella della matrice
+# indirizzo ultima parola =
+# (indirizzo mat) + 4 * ((nRows) * (nColumns + 1) + nColumns)
+la $s0, mat
+addi $t6, $t5, 1
+mul $t6, $t6, $t2
+add $t6, $t6, $t5
+mul $t6, $t6, 4
+add $s0, $s0, $t6
+sw $s1, 0($s0)
+
+quit:
+li $v0, 10
+syscall
---
+++ lab5/lab5_es1.asm
@@ -0,0 +1,33 @@
+.data
+x: .word 3141592653
+
+.text
+.globl main
+.ent main
+main:
+lw $t0, x
+li $t2, 0 # contatore stack frame
+li $t3, '0'
+li $t4, 10
+
+loop_div:
+divu $t0, $t4
+mflo $t0
+mfhi $t1
+addi $sp, $sp, -4
+addi $t2, $t2, 1
+sw $t1, 0($sp)
+bne $t0, $0, loop_div
+
+li $v0, 11
+loop_print:
+lw $t1, 0($sp)
+add $t1, $t1, $t3
+move $a0, $t1
+syscall
+addi $sp, $sp, 4
+addi $t2, $t2, -1
+bne $t2, 0, loop_print
+
+li $v0, 10
+syscall
